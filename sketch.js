b37ba598a3c493f203859da8d3dc90caa614c77b
DanieliStaidel <144032083+DanieliStaidel@users.noreply.github.com>
2023-09-11 11:36:56 -0300

    adicionando arquivo js e css

---
+++ sketch.js
@@ -0,0 +1,15 @@
+}
+ftion setup() {
+crunceateCanvas(450, 450);
+background("red");
+}
+
+function draw() {
+
+stroke("black");
+fill ("white");
+
+// console.log(mouseIspressed);
+
+if(mouseIsPressed) {
+rect(mouseX, mouseY, 20, 35)}
